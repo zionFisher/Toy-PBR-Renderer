721776285d9c4a3108f17432c20b97091264b875
zionFisher <57095348+zionFisher@users.noreply.github.com>
2023-01-19 16:34:29 +0800

    math add constant and utility

---
+++ include/math/constant.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+constexpr float  PI = 3.1415926;
+constexpr double PI_HIGH = 3.1415926535897;
+
+constexpr float  DOUBLE_PI = PI * 2;
+constexpr double DOUBLE_PI_HIGH = PI_HIGH * 2;
+
+constexpr float  EPSILON_HIGH = 1e-5;
+constexpr float  EPSILON_MEDIUM = 1e-7;
+constexpr float  EPSILON_LOW = 1e-10;
+constexpr float  EPSILON = EPSILON_HIGH;
