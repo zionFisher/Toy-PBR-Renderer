bc54c4bf8f5b60bf3cc3fd00aee8ac5e1ccf392f
zionFisher <57095348+zionFisher@users.noreply.github.com>
2023-01-20 17:01:44 +0800

    add matrix unify interface

---
+++ include/math/matrix/matrix.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "float2x2.hpp"
+#include "float3x3.hpp"
+#include "float4x4.hpp"
+
+using matrix2x2 = float2x2;
+using matrix3x3 = float3x3;
+using matrix4x4 = float4x4;
+
+using matrix2 = float2x2;
+using matrix3 = float3x3;
+using matrix4 = float4x4;
+
+using mat2 = float2x2;
+using mat3 = float3x3;
+using mat4 = float4x4;
