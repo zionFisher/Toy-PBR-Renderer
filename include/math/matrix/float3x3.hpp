aed05e96a8f1cb6e41a1f0ca6be8c835558c86f4
zionFisher <57095348+zionFisher@users.noreply.github.com>
2023-01-20 16:44:57 +0800

    Revert "Revert "update math::utility function""
    This reverts commit 14cf40f8bbc8fbdacfff2c91a69041bb9ad6b6c0.

--- include/math/matrix/float3x3.hpp
+++ include/math/matrix/float3x3.hpp
@@ -1,10 +1,17 @@
 #pragma once
 
+#include "float3.hpp"
+
 class float3x3
 {
-private:
+public:
+    float3 rows[3];
 
 public:
-    float3x3();
-    ~float3x3();
+    constexpr float3x3();
+    constexpr float3x3(float3 row1, float3 row2, float3 row3);
+    constexpr float3x3(float3 rows[3]);
+
+public:
+    constexpr float3 &operator[](int index);
 };
