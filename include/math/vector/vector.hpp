7d9936d066770fe114f1507a4bec9e4fd7baaeca
zionFisher <57095348+zionFisher@users.noreply.github.com>
2023-01-20 16:57:52 +0800

    add vector unify interface

---
+++ include/math/vector/vector.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "float2.hpp"
+#include "float3.hpp"
+#include "float4.hpp"
+
+using vector2 = float2;
+using vector3 = float3;
+using vector4 = float4;
+
+using vec2 = float2;
+using vec3 = float3;
+using vec4 = float4;
